369f9bd152a3ebb1b6066f1f7efdfc626fcee86e
Miguel Sebastian Olivera G <96558293+Pk0n@users.noreply.github.com>
2023-04-03 20:53:33 -0600

    Add files via upload
    Updated behavior as the temperature converter was implemented now

--- src/convertidorDeMoneda/Main.java
+++ src/convertidorDeMoneda/Main.java
@@ -2,7 +2,8 @@ package convertidorDeMoneda;
 
 import javax.swing.JOptionPane;
 
-public class Main {
+public class Main 
+{
 		
 	public static void main(String[] args) 
 	{
@@ -24,7 +25,7 @@ public class Main {
 		}
 		else
 		{
-			//Aqui va la opción del conversor de temperaturas
+			TemperatureConverter();
 		}
 		
 	}
@@ -95,7 +96,90 @@ public class Main {
 		switch(nextStep.toString())
 		{
 		case "0":
-			CurrencyConverter();
+			ConverterOption();
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Programa Finalizado");
+			break;
+		}
+	}
+	
+	private static void TemperatureConverter()
+	{
+		String[] temperatureOptions = {"Grados Celsius a Grados Fahrenheit", "Grados Celsius a Kelvin", "Grados Fahrenheit a Grados Celsius", "Grados Fahrenheit a Kelvin", "Kelvin a Grados Celsius", "Kelvin a Grados Fahrenheit"};
+		
+		Object temperatureTarget = JOptionPane.showInputDialog(null, "Seleccione la unidad de temperatura que desea convertir", "Convertidor de temperatura", JOptionPane.QUESTION_MESSAGE, null, temperatureOptions ,0);
+		
+		double result = 0.0;
+		
+		switch(temperatureTarget.toString())
+		{
+			case "Grados Celsius a Grados Fahrenheit":
+	            result = 1.8;
+	            break;
+	        case "Grados Celsius a Kelvin":
+	        	result = 1.0;
+	            break;
+	        case "Grados Fahrenheit a Grados Celsius":
+	        	result = 0.55555555556;
+	            break;
+	        case "Grados Fahrenheit a Kelvin":
+	        	result = 0.55555555556;
+	            break;
+	        case "Kelvin a Grados Celsius":
+	        	result = 1.0;
+	            break;
+	        case "Kelvin a Grados Fahrenheit":
+	        	result = 1.8;
+	            break;
+		}
+		
+		while (true) 
+		{
+	        try 
+	        {
+	            String temperatureInputString = JOptionPane.showInputDialog(null, "Seleccione la cantidad a convertir", "Convertidor de moneda", JOptionPane.QUESTION_MESSAGE);
+	            double temperatureInput = Double.parseDouble(temperatureInputString);double convertedValue = 0.0;
+	            
+	            if (temperatureTarget.toString().equals("Grados Celsius a Grados Fahrenheit")) 
+	            {
+	                convertedValue = (temperatureInput * result) + 32;
+	            } 
+	            else if (temperatureTarget.toString().equals("Grados Celsius a Kelvin")) 
+	            {
+	                convertedValue = temperatureInput + 273.15;
+	            } 
+	            else if (temperatureTarget.toString().equals("Grados Fahrenheit a Grados Celsius")) 
+	            {
+	                convertedValue = (temperatureInput - 32) * result;
+	            } 
+	            else if (temperatureTarget.toString().equals("Grados Fahrenheit a Kelvin")) 
+	            {
+	                convertedValue = (temperatureInput - 32) * result + 273.15;
+	            } 
+	            else if (temperatureTarget.toString().equals("Kelvin a Grados Celsius")) 
+	            {
+	                convertedValue = temperatureInput - 273.15;
+	            } 
+	            else if (temperatureTarget.toString().equals("Kelvin a Grados Fahrenheit")) 
+	            {
+	                convertedValue = (temperatureInput * result) - 459.67;
+	            }
+	            
+	            JOptionPane.showMessageDialog(null, "El valor convertido es: " + convertedValue);
+	            break;
+	        } catch (NumberFormatException e) 
+	        {
+	            JOptionPane.showMessageDialog(null, "Por favor ingrese un valor numérico válido.");
+	        }
+		}
+	    
+		Object nextStep = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+		System.out.println(nextStep);
+		switch(nextStep.toString())
+		{
+		case "0":
+			ConverterOption();
 			break;
 		default:
 			JOptionPane.showMessageDialog(null, "Programa Finalizado");
